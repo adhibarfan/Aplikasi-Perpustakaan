c0a85e904026ab5cb69498e1993701a7395ad702
adhibarfan <adhib.arfan@gmail.com>
2016-11-19 15:08:51 +0700

    Tambah Enum JenisKelamin

---
+++ src/main/java/com/adhib/arfan/aplikasi/perpustakaan/domain/JenisKelamin.java
@@ -0,0 +1,17 @@
+package com.adhib.arfan.aplikasi.perpustakaan.domain;
+
+/**
+ *
+ * @Author Adhib Arfan <adhib.arfan@gmail.com>
+ * @Web <https://adhibarfan.github.io>
+ * @Since Nov 16, 2016
+ * @Time 8:14:05 PM
+ * @Encoding UTF-8
+ * @Project Aplikasi-Perpustakaan
+ * @Package com.adhib.arfan.aplikasi.perpustakaan.domain
+ *
+ */
+public enum JenisKelamin {
+    pria, wanita
+
+}
